5509d305867fb3239acabb9d34f49279a23dad89
Hadi Karimi <karimi1382hadi@gmail.com>
2024-08-28 20:58:01 +0330

    created routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import ContactsPage from "./pages/ContactsPage";
+import ContactPage from "./pages/ContactPage";
+import PageNotFound from "./pages/404";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/contacts" replace />} />
+      <Route path="/contacts" element={<ContactsPage />} />
+      <Route path="/contacts/:id" element={<ContactPage />} />
+      <Route path="/*" element={<PageNotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
